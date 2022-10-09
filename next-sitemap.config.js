d970af7c444362c601dd5643ac5de086bfbc1223
Mariana <ki191_mmv@student.ztu.edu.ua>
2022-10-10 01:05:45 +0300

    add

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'http://localhost:3000',
+    siteUrl: 'https://clinic-site-two.vercel.app',
     generateRobotsTxt: true,
   }
